6a2a56b5f267458545146bee6c7cf92612df7af4
nakanoh <murry.xxx.7@gmail.com>
2020-04-08 21:23:11 +0900

    nuxt.config.jsにPWA設定を追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,55 +1,57 @@
+import pkg from './package'
 
 export default {
   mode: 'spa',
+
   /*
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: pkg.name,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+
   /*
   ** Customize the progress-bar color
   */
   loading: { color: '#fff' },
+
   /*
   ** Global CSS
   */
   css: [
   ],
+
   /*
   ** Plugins to load before mounting the App
   */
   plugins: [
   ],
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
+
   /*
   ** Nuxt.js modules
   */
   modules: [
-    // Doc: https://bootstrap-vue.js.org
-    'bootstrap-vue/nuxt',
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
+    // Doc: https://bootstrap-vue.js.org/docs/
+    'bootstrap-vue/nuxt',
     '@nuxtjs/pwa',
   ],
   /*
   ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
   */
   axios: {
+    // See https://github.com/nuxt-community/axios-module#options
   },
+
   /*
   ** Build configuration
   */
@@ -57,7 +59,12 @@ export default {
     /*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
     }
+  },
+
+  manifest:{
+    name:'bookshelf',
+    lang:'ja'
   }
 }
