5bce16c17c427bce251ef71be88e3021a0b61012
nakanoh <murry.xxx.7@gmail.com>
2020-04-08 20:32:28 +0900

    firebase.jsを修正

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -3,11 +3,13 @@ import firebase from 'firebase'
 if (!firebase.apps.length) {
   firebase.initializeApp({
     apiKey: "AIzaSyBOfzufyY6VG-4VbfvuZfpSC2HV9ykutL8",
-    authDomain: "1:701169814140:web:ee5d3162f41ea9a5b017f9.firebaseapp.com",
+    authDomain: "bookshelf-app-4360d.firebaseapp.com",
     databaseURL: "https://bookshelf-app-4360d.firebaseio.com",
     projectId: "bookshelf-app-4360d",
     storageBucket: "bookshelf-app-4360d.appspot.com",
-    messagingSenderId: "1234567890"
+    messagingSenderId: "701169814140",
+    appId: "1:701169814140:web:ee5d3162f41ea9a5b017f9",
+    measurementId: "G-JC3GJVHWGJ"
   })
 }
 
